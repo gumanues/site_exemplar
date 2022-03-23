25ff260c6a7b60f3d7118b0f83a302dce2720414
Gustavo <gumanues@gmail.com>
2022-03-23 16:17:26 -0300

    Primeiro Commit

---
+++ js/nomesite.js
@@ -0,0 +1,8 @@
+const nomeSite = document.querySelector('#name');
+nomeSite.textContent = 'Site Avaliativo';
+
+const titulo = document.querySelector('#title');
+titulo.textContent = 'Atividade CSS e Bootstrap';
+
+const noty = document.querySelector('#carrinho');
+noty.textContent = '9';
